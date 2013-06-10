055120dba422fa802e3839d91d46989e17ce52f6
afc163 <afc163@gmail.com>
2013-06-10 21:09:21 +0800

    fix config

--- static/config.js
+++ static/config.js
@@ -5,7 +5,7 @@ seajs.config({
         'transit': 'jquery/transit/0.9.9/transit',
         'handlebars': 'gallery/handlebars/1.0.2/handlebars',
         'keymaster': 'gallery/keymaster/1.0.2/keymaster',
-        'position': 'arale/position/1.0.1/position'
+        'position': 'arale/position/1.0.1/position',
         'buzz': 'buzz/buzz',
         'keyboard': 'keyboard/keyboard'
     }
