e20029dd186655e0631af2f8e2e72a21a565f799
afc163 <afc163@gmail.com>
2013-06-10 18:00:36 +0800

    add pop template

--- static/sea-modules/profile/index.js
+++ static/sea-modules/profile/index.js
@@ -3,10 +3,14 @@ define(function(require) {
     var data = require('./data');
     var Ball = require('./ball');
 
-    exports.init = function(type) {
+    exports.initBalls = function(type) {
         for (var i=0; i<data[type].length; i++) {
             new Ball(data[type][i]);
         }
+    }
+
+    exports.clearBalls = function() {
+        $('.ball').remove();
     }
 
 });
