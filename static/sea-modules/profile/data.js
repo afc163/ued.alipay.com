b82a55328a090217f7ae1d7ed409dcc5cec73e0a
pizn <pizner@gmail.com>
2013-08-01 14:27:59 +0800

    update

--- static/sea-modules/profile/data.js
+++ static/sea-modules/profile/data.js
@@ -79,6 +79,12 @@ define({
         name: '贯高',
         message: '前端工程师',
         url: 'http://chuo.me/'
+    }, {
+        name: '右丞',
+        message: '前端工程师'
+    }, {
+        name: '云际',
+        message: '前端工程师'
     }],
     'ur': [{
         name: '白龙',
